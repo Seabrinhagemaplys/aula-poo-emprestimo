da2360840f517614c3be0c9a3953372aadb3ce9b
Seabrinhagemaplys <olivrolegal@gmail.com>
2026-04-15 00:16:49 -0300

    .

--- bancodedados.py
+++ bancodedados.py
@@ -5,12 +5,13 @@ class bancoDeDados:
         self.listaDeEmprestimos: list[Emprestimo] = []
     
     def registrar(self, emprestimoRecebido: Emprestimo):
-        if not self.checarExistenciadeUsuario(emprestimoRecebido: Emprestimo):
-            self.adicionarEmprestimonaLista(emprestimoRecebido: Emprestimo)
-            
+        if not self.checarExistenciadeUsuario(emprestimoRecebido):
+            self.adicionarEmprestimonaLista(emprestimoRecebido)
 
-    def checarExistenciadeUsuario(self, emprestimoRecebido: Emprestimo):
-        for u in 
+    def checarExistenciadeUsuario(self, emprestimoRecebido: Emprestimo) -> bool:
+        for u in self.listaDeEmprestimos:
+            if u.nomeUsuario == emprestimoRecebido.nomeUsuario:
+                return True
     
     def adicionarEmprestimonaLista(self,emprestimoRecebido: Emprestimo):
-        pass
+        self.listaDeEmprestimos.append(emprestimoRecebido)
--- emprestimo.py
+++ emprestimo.py
@@ -1,8 +1,25 @@
+from bancodedados import bancoDeDados
 from abc import ABC, abstractmethod
 
 class Emprestimo(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, nomeUsuario: str, titulo: str):
+        self.checarNomeUsuarioeTitulo(nomeUsuario, titulo)
+        self.adicionarNomeUsuario(nomeUsuario, titulo)
+
+    def checarNomeUsuarioeTitulo(self, nomeUsuario: str, titulo: str):
+        if not isinstance(nomeUsuario, str):
+            raise TypeError("Edição Não é string!")
+        
+        if not isinstance(titulo, str):
+            raise TypeError("Título Não é string!")
+        
+    def adicionarNomeUsuario(self, nomeUsuario: str, titulo: str):
+        self.nomeUsuario = nomeUsuario
+        self.titulo = titulo
+
+    def registrar(self, bancoDados: bancoDeDados):
+        bancoDados.registrar(self)
+        
     
     @abstractmethod
     def registrar(self):
